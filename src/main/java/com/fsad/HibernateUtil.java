a415db3e5b9a8a781f6c66c37be205b1bbcd58a2
YourName <youremail@gmail.com>
2026-03-15 18:37:49 +0530

    Added exp3

---
+++ src/main/java/com/fsad/HibernateUtil.java
@@ -0,0 +1,25 @@
+package com.fsad;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+private static SessionFactory factory;
+
+static {
+
+factory = new Configuration()
+.configure("hibernate.cfg.xml")
+.addAnnotatedClass(Product.class)
+.buildSessionFactory();
+
+}
+
+public static SessionFactory getSessionFactory(){
+
+return factory;
+
+}
+
+}
